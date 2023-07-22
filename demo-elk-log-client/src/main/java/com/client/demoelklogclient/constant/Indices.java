a57fb2b2f95ef13430215b0219e3b83765ef0b67
VardanMatevosyan <vardanmt@gmail.com>
2023-07-22 18:12:20 +0300

    added sorting described by request sortingDto

---
+++ demo-elk-log-client/src/main/java/com/client/demoelklogclient/constant/Indices.java
@@ -0,0 +1,11 @@
+package com.client.demoelklogclient.constant;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = PRIVATE)
+public final class Indices {
+  public static final String USER = "user_6";
+
+}
